0c1b3a69f69725f5d02f1ac71af687c11e2c1b36
bmcart3 <bmcart3@gmail.com>
2020-06-15 17:55:14 -0400

    altered questions.js

--- public/js/questions.js
+++ public/js/questions.js
@@ -1,27 +1,24 @@
 $(document).ready(function() {
-  
-    
-    $('#question-form').on('submit', function(event) {
-        event.preventDefault();
-        var newQuestion = {
-        question: $('#question-input').val().trim(), //change this to username
-        category: $('#category-input').val().trim()
-        };
+  $('#question-form').on('submit', function(event) {
+    event.preventDefault();
+    var newQuestion = {
+      question: $('#question-input').val().trim(), //change this to username
+      category: $('#category-input').val().trim()
+    };
 
-        // Does a post to the question route.
-        $.post('/api/questions', newQuestion)
-            .then(function() {
-            console.log('New question added');
-            location.reload();
-            // If there's an error, handle it by throwing up a bootstrap alert
-        });
+    // Does a post to the question route.
+    $.post('/api/questions', newQuestion)
+      .then(function() {
+        console.log('New question added');
+        location.reload();
+        // If there's an error, handle it by throwing up a bootstrap alert
+      });
 
-        $('#question-input').val('');
-        $('#category-input').val('');
-    });
+    $('#question-input').val('');
+    $('#category-input').val('');
+  });
 
-    $('#answer-update').on('click', function(event) {
-        event.preventDefault();
-    })
-    
+  $('#answer-update').on('click', function(event) {
+    event.preventDefault();
+  });
 });
