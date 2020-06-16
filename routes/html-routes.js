e33eb64ea678e928146acc347b351252c982cd4b
bmcart3 <bmcart3@gmail.com>
2020-06-16 16:56:32 -0400

    fixed onclick for answer submission and added id to answer input field

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,41 +15,41 @@ const db = require('../models');
 
 module.exports = function(app) {
 
-  app.get('/', function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      return res.redirect('/questions'); //was originally members
-    }
-    res.render('index');
-  });
+     app.get('/', function(req, res) {
+          // If the user already has an account send them to the members page
+          if (req.user) {
+               return res.redirect('/questions'); //was originally members
+          }
+          res.render('index');
+     });
 
-  app.get('/login', function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      return res.redirect('/questions');
-    }
-    res.render('login');
-  });
+     app.get('/login', function(req, res) {
+          // If the user already has an account send them to the members page
+          if (req.user) {
+               return res.redirect('/questions');
+          }
+          res.render('login');
+     });
 
-  app.get('/signup', function(req, res) {
-    res.render('signup');
-  });
+     app.get('/signup', function(req, res) {
+          res.render('signup');
+     });
 
-  app.get('/index', function(req, res) {
-    res.render('index');
-  });
+     app.get('/index', function(req, res) {
+          res.render('index');
+     });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get('/questions', isAuthenticated, function(req, res) {
-    db.Question.findAll({})
-      .then(questions => {
-        res.render('questions', { questions: questions });
-      });
-  });
+     // Here we've add our isAuthenticated middleware to this route.
+     // If a user who is not logged in tries to access this route they will be redirected to the signup page
+     app.get('/questions', isAuthenticated, function(req, res) {
+          db.Question.findAll({})
+               .then(questions => {
+                    res.render('questions', { questions: questions });
+               });
+     });
 
-  //isAuthenticated above is the middleware being inserted into just a specific route.
-  //On line 27, it is checking to see if the user is authenticated before sending back the questions page.
+     //isAuthenticated above is the middleware being inserted into just a specific route.
+     //On line 27, it is checking to see if the user is authenticated before sending back the questions page.
 };
 
 
