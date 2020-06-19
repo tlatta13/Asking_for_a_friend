5196bfba0a0d6b3cd08c2d1e20440f80aa81fdc5
Emily Renegar <egrenegar@gmail.com>
2020-06-18 23:59:31 -0400

    added functionality to display username, and  more refactoring

--- public/js/questions.js
+++ public/js/questions.js
@@ -1,11 +1,15 @@
+let $title = $('#display-question-title');
+let $content = $('#display-question-content');
+let $username = $('#display-username');
+let $created = $('#display-createdAt');
+let $answerBtn = $('#answer-add');
+let $answerList = $('answer-list');
+
 $(document).ready(function () {
   // When the page loads, retrieve the last selected question from local storage and render it
   // If local storage is empty, make an api request to render a question from database
   // If nothing in database, display a message in the browser
   const renderLastQuestion = () => {
-    let $title = $('#display-question-title');
-    let $content = $('#display-question-content');
-    let $created = $('#display-createdAt');
 
     const checkLocalStorage = () => {
       let id = localStorage.getItem('questionId');
@@ -13,10 +17,12 @@ $(document).ready(function () {
         $.get('/api/questions/' + id + '/answers')
           .then(function (response) {
             console.log(response);
+            // add a data-id attribute to the submit button in the answer form
+            $($answerBtn).attr('data-id', response[0].id);
+            // render the question details
             $title.text(response[0].title);
-            // render the question content
             $content.text(response[0].question);
-            // render time question was asked
+            $username.text('Posted by: ' + response[0].username);
             $created.text(response[0].createdAt);
             // I changed this index to just 0 since there will only ever be one item in the array
             let answers = response[0].Answers;
@@ -25,17 +31,18 @@ $(document).ready(function () {
             // create a new <li> for each recent answer and append it to the list
             recentAnswers.forEach((answer) => {
               let newli = $('<li>').text(answer.answer);
-              $('#answer-list').append(newli);
+              $($answerList).append(newli);
             });
           });
       } else {
         $.get('/api/questions/' + 1 + '/answers')
           .then(function (response) {
             console.log(response);
+            $($answerBtn).attr('data-id', response[0].id);
+            // render the question details
             $title.text(response[0].title);
-            // render the question content
             $content.text(response[0].question);
-            // render time question was asked
+            $username.text('Posted by: ' + response[0].username);
             $created.text(response[0].createdAt);
             // I changed this index to just 0 since there will only ever be one item in the array
             let answers = response[0].Answers;
@@ -44,7 +51,7 @@ $(document).ready(function () {
             // render a new <li> for each recent answer
             recentAnswers.forEach((answer) => {
               let newli = $('<li>').text(answer.answer);
-              $('#answer-list').append(newli);
+              $($answerList).append(newli);
             });
           });
       }
@@ -71,23 +78,31 @@ $(document).ready(function () {
   // When user submits a new question...
   $('#question-form').on('click', function (event) {
     event.preventDefault();
-    let newQuestion = {
-      title: $('#title-input').val().trim(),
-      question: $('#question-input').val().trim()
+    const newQuestion = (username) => {
+      const newQuestion = {
+        title: $('#title-input').val().trim(),
+        question: $('#question-input').val().trim(),
+        username: username
+      };
+      // Does a post to the question route.
+      $.post('/api/questions', newQuestion)
+        .then(function (response) {
+          localStorage.setItem('questionId', response.id);
+          location.reload();
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
     };
 
-    // Does a post to the question route.
-    $.post('/api/questions', newQuestion)
-      .then(function () {
-        location.reload();
-      })
-      .catch(function (err) {
-        console.log(err);
+    $.get('/api/user_data')
+      .then(function (response) {
+        newQuestion(response.username);
       });
   });
 
   // When user adds a new answer...
-  $('#answer-add').on('click', function (event) {
+  $($answerBtn).on('click', function (event) {
     let dataId = $(this).attr('data-id');
     let id = (typeof dataId === 'undefined') ? 1 : dataId;
     event.preventDefault();
@@ -98,7 +113,7 @@ $(document).ready(function () {
       .then(function (response) {
         // create a new <li> and append it to the <ol> in questions.handlebars
         let newAnswer = $('<li>' + response.answer + '</li>');
-        $('#answer-list').append(newAnswer);
+        $($answerList).append(newAnswer);
         $('#answer-input').val('');
       });
   });
@@ -107,24 +122,19 @@ $(document).ready(function () {
   $('a.panel-block').on('click', function (event) {
     event.preventDefault();
     // remove the <li>s from the <ol> in "Answers"
-    $('#answer-list').empty();
-    // Setting our variables
-    let $title = $('#display-question-title');
-    let $content = $('#display-question-content');
-    let $created = $('#display-createdAt');
+    $($answerList).empty();
     // get the data-id from questions.handlebars
     let id = $(this).attr('data-id');
     // add a data-id attribute to the submit button in the answer form
-    $('#answer-add').attr('data-id', id);
+    $($answerBtn).attr('data-id', id);
 
     // Render the questions and answers to the browser
     $.get('/api/questions/' + id + '/answers')
       .then(function (response) {
-        // render the question title
+        // render the question details
         $title.text(response[0].title);
-        // render the question content
         $content.text(response[0].question);
-        // render time question was asked
+        $username.text('Posted by: ' + response[0].username);
         $created.text(response[0].createdAt);
         // $created.text(moment.utc(response[i].createdAt.local().format('LLL')));
 
@@ -137,7 +147,7 @@ $(document).ready(function () {
         // render a new <li> for each recent answer
         recentAnswers.forEach((answer) => {
           let newli = $('<li>').text(answer.answer);
-          $('#answer-list').append(newli);
+          $($answerList).append(newli);
         });
       });
   });
